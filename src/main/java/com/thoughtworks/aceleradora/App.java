f73524f802c9897590a4172f6e7cb6d1da06b723
stefanikopp <stefani.kopp@acad.pucrs.br>
2019-09-24 14:41:34 -0300

    par05/@BrayanArgradem: Faz teste FizzBuzz

--- src/main/java/com/thoughtworks/aceleradora/App.java
+++ src/main/java/com/thoughtworks/aceleradora/App.java
@@ -3,6 +3,7 @@ package com.thoughtworks.aceleradora;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.IllegalFormatCodePointException;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -12,8 +13,11 @@ public class App {
 
         Scanner in = new Scanner(System.in);
 
-        System.out.println("      Ola Empreendedor   ");
-        System.out.println("  Nós somos a EasyBeasy e vamos ajudar a sua empresa! ");
+        System.out.println("                  Olá Empreendedor                      ");
+        System.out.println("    Nós somos a EasyBeasy e vamos ajudar a sua empresa! ");
+        System.out.println("*********************************************************");
+        System.out.println("          RESPONDA com  S para SIM  e  N para Não        ");
+        System.out.println("*********************************************************");
         System.out.println(" Você está com problemas na sua empresa? \n");
         String resp = in.nextLine();
         if (resp.equals("N")){
@@ -23,9 +27,22 @@ public class App {
             resp = in.nextLine();
             perguntasFinanceiro(resp);
             if (resp.equals("N")){
-                System.out.println("COLETORA - Você está com problemas de organização na sua empresa?\n");
+                System.out.println(" COLETORA - Você está com problemas de organização na sua empresa?\n");
                 resp = in.nextLine();
                 perguntasOrg(resp);
+                if (resp.equals("N")){
+                    System.out.println(" COLETORA - Você está com problemas relacionados a parte estratégica da sua empresa?\n");
+                    resp = in.nextLine();
+                    perguntasEstrategica(resp);
+                    if (resp.equals("N")){
+                        System.out.println(" Bem Empreendedor \n  Não identificamos nenhum problema nesse momento \n  Você que fazer mesmo assim o Diagnóstico? ");
+                        resp = in.nextLine();
+                        if (resp.equals("S")){
+                           // perguntasFinanceiro(resp);
+                            System.out.println(" EM CONTRUÇÃO !!!!");
+                        }
+                    }
+                }
             }
         }
 
@@ -45,27 +62,27 @@ public class App {
             respF = in.nextLine();
             if (respF.equals("N")){
                 System.out.println(" Identificamos que você tem problemas em organizar as finanças da empresa \n " +
-                                   "Acesse o link www.easybeasy.com.br/solucao-financeiro \n para obter a solução");
+                                   "Acesse o link www.easybeasy.com.br/solucao-financeiro para obter a solução");
             }
-            if (respF.equals("S")){
-                System.out.println(" VALIDAÇÃO 1 - Você reconhece que suas horas de trabalho também entram nos custos da empresa?\n");
-                respF = in.nextLine();
-                if (respF.equals("N")){
-                    System.out.println(" Identificamos que você tem problemas em organizar as finanças da empresa \n " +
-                                       "Acesse o link www.easybeasy.com.br/solucao-financeiro \n para obter a solução");
+                if (respF.equals("S")){
+                     System.out.println(" VALIDAÇÃO 1 - Você reconhece que suas horas de trabalho também entram nos custos da empresa?\n");
+                     respF = in.nextLine();
+                     if (respF.equals("N")){
+                        System.out.println(" Identificamos que você tem problemas em organizar as finanças da empresa \n " +
+                                           "Acesse o link www.easybeasy.com.br/solucao-financeiro para obter a solução");
+                     }
+                          if (respF.equals("S")){
+                           System.out.println(" VALIDAÇÃO 2 - Você sabe quais são os custos fixos e variáveis da sua empresa?\n");
+                           respF = in.nextLine();
+                               if (respF.equals("N")){
+                                    System.out.println(" Identificamos que você tem problemas em organizar as finanças da empresa \n " +
+                                                       "Acesse o link www.easybeasy.com.br/solucao-financeiro  para obter a solução");
+                               }
+                            }
                 }
-                     if (respF.equals("S")){
-                      System.out.println(" VALIDAÇÃO 2 - Você sabe quais são os custos fixos e variáveis da sua empresa?\n");
-                      respF = in.nextLine();
-                         if (respF.equals("N")){
-                             System.out.println(" Identificamos que você tem problemas em organizar as finanças da empresa \n " +
-                                                "Acesse o link www.easybeasy.com.br/solucao-financeiro \n para obter a solução");
-                         }
-                      }
-            }
         }
 
-        return " SEGUE O FLUXO! ";
+        return " ";
     }
     public static String perguntasOrg(String resp){
         Scanner in = new Scanner(System.in);
@@ -76,39 +93,81 @@ public class App {
             respP = in.nextLine();
                 if (respP.equals("N")){
                 System.out.println(" Identificamos que você tem problema na organização da sua empresa \n " +
-                                   "Acesse o link www.easybeasy.com.br/solucao-organização \n para obter a solução");
+                                   "Acesse o link www.easybeasy.com.br/solucao-organizacao para obter a solução");
                 }
                     if(respP.equals("S")){
                     System.out.println(" VALIDAÇÃO 1 - Você tem uma organização de tempo e tarefas?\n");
                     respP = in.nextLine();
                         if (respP.equals("N")){
                         System.out.println(" Identificamos que você tem problema na organização da sua empresa \n " +
-                                           "Acesse o link www.easybeasy.com.br/solucao-organização \n para obter a solução");
+                                           "Acesse o link www.easybeasy.com.br/solucao-organizacao para obter a solução");
                         }
                            if(respP.equals("S")){
                            System.out.println(" VALIDAÇÃO 2 - Você organiza a semana em tarefas e metas a serem cumpridas?\n");
                            respP = in.nextLine();
                                 if (respP.equals("N")){
                                 System.out.println(" Identificamos que você tem problema na organização da sua empresa \n " +
-                                                   "Acesse o link www.easybeasy.com.br/solucao-organização \n para obter a solução");
+                                                   "Acesse o link www.easybeasy.com.br/solucao-organizacao para obter a solução");
                                 }
                                    if(respP.equals("S")){
                                    System.out.println(" VALIDAÇÃO 3 - Você utiliza alguma ferramenta ou estratégia para organizar a sua semana?\n");
                                    respP = in.nextLine();
                                        if (respP.equals("N")){
                                            System.out.println(" Identificamos que você tem problema na organização da sua empresa \n " +
-                                                              "Acesse o link www.easybeasy.com.br/solucao-organização \n para obter a solução");
+                                                              "Acesse o link www.easybeasy.com.br/solucao-organizacao  para obter a solução");
                                        }
-                    }
-                }
+                                   }
+                           }
 
-            }
+                    }
 
         }
 
 
-        return " SEGUE O FLUXO! ";
+        return "";
 
+    }
+
+    public static String perguntasEstrategica(String resp){
+        Scanner in = new Scanner(System.in);
+        String respM = "";
+
+        if (resp.equals("S")){
+            System.out.println(" CABEÇA - Você tem conhecimento sobre o público que consome o seu produto/serviço?\n ");
+            respM = in.nextLine();
+            if (respM.equals("N")){
+                System.out.println(" Identificamos que você tem problema estratégico na sua empresa \n " +
+                                   "Acesse o link www.easybeasy.com.br/solucao-estrategico para obter a solução");
+            }
+                if (respM.equals("S")){
+                    System.out.println(" VALIDAÇÃO 1 - Você tem conhecimento sobre o público que consome o seu produto/serviço?\n");
+                    respM = in.nextLine();
+                    if (respM.equals("N")){
+                        System.out.println(" Identificamos que você tem problema estratégico na sua empresa \n " +
+                                           "Acesse o link www.easybeasy.com.br/solucao-estrategico  para obter a solução");
+                    }
+                        if (respM.equals("S")){
+                           System.out.println(" VALIDAÇÃO 2 - Você compreende as dores e as necessidades do seu cliente, de acordo com as características dele?\n");
+                            respM = in.nextLine();
+                            if (respM.equals("N")){
+                                System.out.println(" Identificamos que você tem problema estratégico na sua empresa \n " +
+                                                   "Acesse o link www.easybeasy.com.br/solucao-estrategico  para obter a solução");
+                            }
+                                if (respM.equals("S")){
+                                   System.out.println(" VALIDAÇÃO 3 - Você utilizou algum método ou ferramenta para entender melhor o seu cliente?\n");
+                                   respM = in.nextLine();
+                                    if (respM.equals("N")){
+                                        System.out.println(" Identificamos que você tem problema estratégico na sua empresa \n " +
+                                                           "Acesse o link www.easybeasy.com.br/solucao-estrategico  para obter a solução");
+                                    }
+
+                                }
+                        }
+                }
+        }
+
+
+        return "";
     }
 
 }
