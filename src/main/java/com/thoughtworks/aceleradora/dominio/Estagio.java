0b926f797f2205223fb5b6d71fbf958538762bc9
cassia11 <cassiagomes0102@gmail.com>
2019-09-24 15:09:56 -0300

    par01/@JefersonCruz3, @cassia11: cria metodo de adicionar pergunta na classe estagio

--- src/main/java/com/thoughtworks/aceleradora/dominio/Estagio.java
+++ src/main/java/com/thoughtworks/aceleradora/dominio/Estagio.java
@@ -7,4 +7,38 @@ public class Estagio {
 
     private List<Pergunta> perguntas= new ArrayList<>();
     private String solucao;
+    private Categoria categoria;
+
+    public Estagio( String solucao, Categoria categoria) {
+        this.solucao = solucao;
+        this.categoria = categoria;
+    }
+
+    public List<Pergunta> getPerguntas() {
+        return perguntas;
+    }
+
+    public void setPerguntas(List<Pergunta> perguntas) {
+        this.perguntas = perguntas;
+    }
+
+    public String getSolucao() {
+        return solucao;
+    }
+
+    public void setSolucao(String solucao) {
+        this.solucao = solucao;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public void adicionaPergunta(Pergunta pergunta){
+        perguntas.add(pergunta);
+    }
 }
