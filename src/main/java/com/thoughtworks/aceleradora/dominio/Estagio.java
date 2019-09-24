1f91dbeb2a9413c5a62987f0221d8a27951dc3dd
cassia11 <cassiagomes0102@gmail.com>
2019-09-24 13:49:29 -0300

    par01/@cassia11, @JefersonCruz3: cria classe estágio

---
+++ src/main/java/com/thoughtworks/aceleradora/dominio/Estagio.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.aceleradora.dominio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Estagio {
+
+    private List<Pergunta> perguntas= new ArrayList<>();
+
+}
