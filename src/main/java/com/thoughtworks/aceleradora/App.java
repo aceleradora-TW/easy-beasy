da2b55f7d43e41df90a0c40cd1ea653873f2056f
Queiroxzz <rafaelaqueiroz181@gmail.com>
2019-09-25 15:56:13 -0300

    dojo16/@todos: Inicia a implementação do menu

--- src/main/java/com/thoughtworks/aceleradora/App.java
+++ src/main/java/com/thoughtworks/aceleradora/App.java
@@ -1,12 +1,40 @@
 package com.thoughtworks.aceleradora;
 
-import org.springframework.boot.SpringApplication;
+import com.thoughtworks.aceleradora.dominio.Diagnostico;
+import com.thoughtworks.aceleradora.dominio.Nivel;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
 
 @SpringBootApplication
 public class App {
 
+    public static void gerenciarNiveis() {
+
+
+//        switch (opcao) {
+//            case 1:
+//                novoNivel.criaNivel();
+//        }
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        //SpringApplication.run(App.class, args);
+        Scanner entrada = new Scanner(System.in);
+
+        Diagnostico novoDiagnostico = new Diagnostico();
+        Nivel novoNivel = new Nivel();
+
+        System.out.println("Boas vindas, {Administrador}");
+
+        int opcao;
+
+        System.out.println("1 - Gerenciar niveis.");
+        opcao = entrada.nextInt();
+
+        switch (opcao) {
+            case 1:
+        }
     }
 }
