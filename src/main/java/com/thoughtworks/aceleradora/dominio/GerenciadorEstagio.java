37566b846377d9a3e914b53b29f0b55a5b16db7e
cassia11 <cassiagomes0102@gmail.com>
2019-09-30 15:49:35 -0300

    dojo16/@adrio, @stefani e @cassia: cria classe gerenciadorEstagio, instancia Scanner e classe subnivel e cria construtor

---
+++ src/main/java/com/thoughtworks/aceleradora/dominio/GerenciadorEstagio.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.aceleradora.dominio;
+
+import java.util.Scanner;
+
+public class GerenciadorEstagio {
+    private Scanner entrada;
+    private Subnivel novoSubnivel;
+
+    public GerenciadorEstagio(Scanner entrada, Subnivel novoSubnivel) {
+        this.entrada = entrada;
+        this.novoSubnivel = novoSubnivel;
+
+
+    }
+}
