83e7c61d7456972e88a1d197d8701ea736d460fd
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2019-10-01 10:54:36 -0300

    area/ @ingridDuarte, @brayanargradem: Implementa funcionalidade de adicionar área

--- src/main/java/com/thoughtworks/aceleradora/dominio/GerenciadorArea.java
+++ src/main/java/com/thoughtworks/aceleradora/dominio/GerenciadorArea.java
@@ -1,25 +1,42 @@
 package com.thoughtworks.aceleradora.dominio;
 
+import org.springframework.web.context.request.AbstractRequestAttributesScope;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class GerenciadorArea {
     private Scanner entrada;
+    private List<Area> areas = new ArrayList<>();
 
-    public GerenciadorArea(Scanner entrada){
+    public GerenciadorArea(Scanner entrada) {
         this.entrada = entrada;
     }
 
-    public void menuArea() {
+    public Area criarArea() {
+        System.out.println("Digite o nome desejado para sua área");
+        String nome = entrada.nextLine();
+
+        return new Area(nome);
+    }
+
+    public void adicionarArea() {
+        areas.add(criarArea());
+    }
+
+       public void menuArea() {
         int opcao;
         do {
             System.out.println("Digite a opção desejada");
-            System.out.println("1 - Criar area");
+            System.out.println("1 - Adicionar área");
             System.out.println("0 - Sair");
             opcao = entrada.nextInt();
             entrada.nextLine();
 
             switch (opcao) {
                 case 1:
+                    adicionarArea();
                     break;
                 case 0:
                     System.out.println("Retornando ao menu inicial");
