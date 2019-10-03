5673ee510fb996c4952d6615de753df01db7ddde
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2019-10-03 10:40:52 -0300

    questionario/@ingridDuarte, @GabrieRabelo, @EduardaSchuler: Cria um diagnostico com dados mockados

--- src/main/java/com/thoughtworks/aceleradora/dominio/Questionario.java
+++ src/main/java/com/thoughtworks/aceleradora/dominio/Questionario.java
@@ -6,15 +6,7 @@ import java.util.List;
 public class Questionario {
 
     public static void main(String[] args) {
-        Diagnostico diagnostico = new Diagnostico();
         List<Pergunta> novaListaPergunta = new ArrayList<>();
-        List<Estagio> novaListaEstagio = new ArrayList<>();
-
-
-        Nivel nivel = new Nivel(1, "A");
-        Subnivel subnivel = new Subnivel();
-        Area areaOrganizacao = new Area("Organização");
-
         Pergunta primeiraPergunta = new Pergunta(1, "Você tem uma organização de tempo e tarefas?");
         novaListaPergunta.add(primeiraPergunta);
         Pergunta segundaPergunta = new Pergunta(2, "Você geralmente esquece de coisas que precisam ser feitas?");
@@ -23,17 +15,29 @@ public class Questionario {
         novaListaPergunta.add(terceiraPergunta);
         Pergunta quartaPergunta = new Pergunta(4, "Você utiliza alguma ferramenta ou estratégia para organizar a sua semana?");
         novaListaPergunta.add(quartaPergunta);
+
         Estagio estagio1 = new Estagio();
         estagio1.setPerguntas(novaListaPergunta);
-
         estagio1.setSolucao(
-                        "Com base nas suas respostas realizei o diagnóstico e encontrei os seguintes problemas: " + "\n" +
+                "Com base nas suas respostas realizei o diagnóstico e encontrei os seguintes problemas: " + "\n" +
                         "Podemos melhorar alguns pontos na sua gestão de tempo e tarefas. A solução consistirá em como organizar " + "\n" +
                         "suas atividades, definir prazos e metas, além da indicação de algumas ferramentas que poderão complementar a " +
                         "sua rotina. \n" +
                         "https://medium.com/@easybeasybr/gestao-de-tempo-e-tarefas-p1-12a51de24f75\n");
 
+        List<Estagio> novaListaEstagio = new ArrayList<>();
         novaListaEstagio.add(estagio1);
 
+        Area areaOrganizacao = new Area("Organização");
+
+        Subnivel subnivel = new Subnivel(1, areaOrganizacao, novaListaEstagio);
+        List<Subnivel> listaSubnivel = new ArrayList<>();
+        listaSubnivel.add(subnivel);
+
+        Nivel nivel = new Nivel(1, "A", listaSubnivel);
+        List<Nivel> listaNiveis = new ArrayList<>();
+        listaNiveis.add(nivel);
+
+        Diagnostico diagnostico = new Diagnostico(listaNiveis);
     }
 }
