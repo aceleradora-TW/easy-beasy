23b6b30ca25458be41fbd1ab3343d1e58118cd2e
cassia11 <cassiagomes0102@gmail.com>
2019-10-07 11:07:44 -0300

    CRUD-Validacao-Nivel/@EvertonFernandes93 @Cassia11: cria método que validaNome

--- src/main/java/com/thoughtworks/aceleradora/dominio/ValidadorNivel.java
+++ src/main/java/com/thoughtworks/aceleradora/dominio/ValidadorNivel.java
@@ -10,6 +10,7 @@ public class ValidadorNivel extends ValidacoesBase {
         String nomeRecebido = nome;
 
         validaOrdem(ordemRecebida);
+        validaNome(nomeRecebido);
     }
     private void validaOrdem(String ordem) {
         try {
@@ -18,37 +19,11 @@ public class ValidadorNivel extends ValidacoesBase {
             erros.add("Ordem deve ser um número!");
         }
     }
+    private void validaNome(String nome) {
+        if(nome.isEmpty()){
+            erros.add("Nome do nível não pode estar em branco!");
+        }
+    }
 
 }
 
-//    public int defineOrdem(GerenciadorNivel gerenciadorNivel, String ordemString){
-//
-//        while (!ValidacoesBase.ehNumero(ordemString)) {
-//            System.out.println("Ordem inexistente. Digite um número válido!");
-//            ordemString = entrada.nextLine();
-//            if (ordemString.equalsIgnoreCase("cancelar")) {
-//                gerenciadorNivel.menuNivel();
-//            }
-//        }
-//        return Integer.parseInt(ordemString);
-//    }
-//
-//    public String defineNome(){
-//        boolean nomeInvalido = false;
-//        String nome;
-//        do {
-//            System.out.println("Digite o nome do nível ('cancelar' para sair do programa)");
-//            nome = entrada.nextLine();
-//            if ((Validacoes.nomeInvalido(nome))) {
-//                System.out.println("Nome invalido");
-//                nomeInvalido = true;
-//            } else {
-//                nomeInvalido = Validacoes.nomeRepetido(novoDiagnostico.getNiveis(), nome);
-//                if (nomeInvalido) System.out.println("Este nome já existe, escolha outro nome.");
-//            }
-//        } while (nomeInvalido);
-//
-//        if (nome.equalsIgnoreCase("cancelar")) return null;
-//    }
-//
-//}
