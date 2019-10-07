6fa748ab11a835850ca83a40d04d16f29e223f84
cassia11 <cassiagomes0102@gmail.com>
2019-10-07 10:44:13 -0300

    CRUD-Validacao-Nivel/@cassia11: Implementa método validar que recebe nome e ordem como parametros na classe Validaçoes

--- src/main/java/com/thoughtworks/aceleradora/dominio/Validacoes.java
+++ src/main/java/com/thoughtworks/aceleradora/dominio/Validacoes.java
@@ -1,8 +1,14 @@
 package com.thoughtworks.aceleradora.dominio;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Validacoes {
+public abstract class Validacoes {
+
+    protected final List<String> erros= new ArrayList<>();
+
+    public abstract void validar(String ordem, String nome);
+
 
     public static boolean ehNumero(String entrada) {
         try {
@@ -13,17 +19,17 @@ public class Validacoes {
         return true;
     }
 
-    public static boolean nomeRepetido(List<Nivel> niveis, String nome) {
-        for (Nivel nivelAux : niveis) {
-            if (nivelAux.getNome().equalsIgnoreCase(nome)) return true;
-        }
-        return false;
-    }
-
-    public static boolean nomeInvalido(String nome) {
-        if (nome.startsWith(" ") || nome.isEmpty()) return true;
-        return false;
-
-    }
+//    public static boolean nomeRepetido(List<Nivel> niveis, String nome) {
+//        for (Nivel nivelAux : niveis) {
+//            if (nivelAux.getNome().equalsIgnoreCase(nome)) return true;
+//        }
+//        return false;
+//    }
+//
+//    public static boolean nomeInvalido(String nome) {
+//        if (nome.startsWith(" ") || nome.isEmpty()) return true;
+//        return false;
+//
+//    }
 
 }
