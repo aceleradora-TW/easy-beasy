a11ffef034ea8ae8d55c3f3cc3979ac40f9bd6e9
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-10-07 11:24:20 -0300

    dojo16-questionario/@todos:Adapta o fluxo pra mais de um estagio

--- src/main/java/com/thoughtworks/aceleradora/dominio/AppQuestionario.java
+++ src/main/java/com/thoughtworks/aceleradora/dominio/AppQuestionario.java
@@ -11,35 +11,43 @@ public class AppQuestionario {
 
         System.out.println("Olá, boas-vindas. Somos a EasyBeasy.");
 
-        for (Nivel nivel : novoDiagnostico.getNiveis()) {
-            for (Subnivel subnivel : nivel.getSubnivel()) {
-                for (Estagio estagio : subnivel.getEstagios()) {
-                    int cont = 0;
-                    for (Pergunta pergunta : estagio.getPerguntas()) {
-                        String resposta;
-                        do {
-                            System.out.println(pergunta.getDescricao());
-                            resposta = entrada.nextLine();
-                            if (!resposta.equalsIgnoreCase("nao") && !resposta.equalsIgnoreCase("sim")) {
-                                System.out.println("Resposta inválida.");
+            for (Nivel nivel : novoDiagnostico.getNiveis()) {
+                for (Subnivel subnivel : nivel.getSubnivel()) {
+                    for (Estagio estagio : subnivel.getEstagios()) {
+                        int cont = 0;
+                        for (Pergunta pergunta : estagio.getPerguntas()) {
+                            String resposta;
+                            do {
+                                System.out.println(pergunta.getDescricao());
+                                resposta = entrada.nextLine();
+                                if (!resposta.equalsIgnoreCase("nao") && !resposta.equalsIgnoreCase("sim")) {
+                                    System.out.println("Resposta inválida.");
+                                }
+                            } while (!resposta.equalsIgnoreCase("nao") && !resposta.equalsIgnoreCase("sim"));
+                            if (resposta.equalsIgnoreCase("nao")) {
+                                cont++;
                             }
-                        } while (!resposta.equalsIgnoreCase("nao") && !resposta.equalsIgnoreCase("sim"));
-                        if (resposta.equalsIgnoreCase("nao")) {
-                            cont++;
+                            if (cont == 2) {
+                                break;
+                            }
                         }
-                        if (cont == 3) {
+                        if (cont > 0) {
                             System.out.println(estagio.getSolucao());
-                            break;
+                            System.out.println("Deseja continuar o diagnóstico? ");
+                            String resposta;
+                            do {
+                                resposta = entrada.nextLine();
+                                if (!resposta.equalsIgnoreCase("nao") && !resposta.equalsIgnoreCase("sim")) {
+                                    System.out.println("Resposta inválida.");
+                                }
+                            } while (!resposta.equalsIgnoreCase("nao") && !resposta.equalsIgnoreCase("sim"));
+
+                            if (resposta.equalsIgnoreCase("nao")){
+                                return;
+                            }
                         }
-                    }
-                    if (cont > 0) {
-                        System.out.println(estagio.getSolucao());
-                        break;
-                    } else {
-                        System.out.println("Congratulations Steve Jobs");
                     }
                 }
             }
-        }
     }
 }
